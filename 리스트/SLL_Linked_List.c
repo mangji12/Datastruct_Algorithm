64cc4fe9a06f7ad0d4d8be68f89594ba0d5950f7
mangji12 <dhckddls12@naver.com>
2024-09-09 03:37:46 +0900

    노드 삭제 함수 구현

--- 리스트/SLL_Linked_List.c
+++ 리스트/SLL_Linked_List.c
@@ -17,11 +17,18 @@
 // 자유 저장소(힙)
 Node* CreateNode_heap(int data)
 {
-  Node* NewNode = (Node*)malloc(sizeof(Node));
-  NewNode->data = data;
-  NewNode->NextNode = NULL;
+ Node* NewNode = (Node*)malloc(sizeof(Node));
+ NewNode->data = data;
+ NewNode->NextNode = NULL;
+ return NewNode;
+}
 
-  return NewNode;
+// 링크드 리스트 생성 함수
+LinkedList* Linked_list()
+{
+  LinkedList* linked_list = (LinkedList*)malloc(sizeof(LinkedList));
+  linked_list->Head = NULL;
+  return linked_list;
 }
 
 // 노드 소멸 함수
@@ -31,31 +38,58 @@ Node* Destroy_Node(Node* Node)
 }
 
 // 노드 추가 함수(append)
-Node* AppendNode(Node** Head, Node* NewNode) // 링크드 리스트 헤드 노드의 주소를 넘김. 싱글 포인터로 넘기게 되면 원본 포인터의 복사본 자체(데이터)를 넘기게 되지만, 더블포인터로 넘기면 원본 포인터의 "주소"를 넘김
+Node* AppendNode(LinkedList* list, Node* NewNode) // 링크드 리스트 헤드 노드의 주소를 넘김. 싱글 포인터로 넘기게 되면 원본 포인터의 복사본 자체(데이터)를 넘기게 되지만, 더블포인터로 넘기면 원본 포인터의 "주소"를 넘김
 {
-  if ((*Head) == NULL) // 링크드 리스트의 목록이 없을 때 NewNode를 첫 헤드로 사용.
-    (*Head) = NewNode;
-  else // 헤드가 존재한다면
+  if (list->Head == NULL)
+    list->Head = NewNode;
+  else
   {
-    Node* current = (*Head); // Head의 주소를 복사 -> 원본이 수정됨
-    while (current->NextNode != NULL) // 다음 노드가 NULL일 동안~ 
-      current = current->NextNode; 
-    current->NextNode = NewNode; // 마지막에 붙이기
-  } 
+    Node* current = list->Head; // 현재 노드를 헤드로 초기화
+    while (current->NextNode != NULL)
+      current = current->NextNode;
+    list->Head = current;
+  }
+  return list->Head;
 }
 
 // 노드 탐색 함수
-Node* SearchNode(Node* Head, int idx)
+Node* SearchNode(LinkedList* list, int idx)
 {
-  Node* current = Head;
-  while (current != NULL && (--idx) >= 0)
+  Node* current = list->Head;
+  while (current != NULL && idx-- > 0)
     current = current->NextNode;
-
+  
   return current;
 }
 
 // 노드 삭제 함수
+Node* DeleteNode(LinkedList* list, Node* delNode)
+{
+  Node* current = list->Head;
+  Node* previous = NULL;
+
+  while (current != NULL && current != delNode)
+  {
+    previous = current;
+    current = current->NextNode;
+  }
+  
+  if (current != NULL)
+  {
+    if (previous == NULL)
+      list->Head = current->NextNode;
+    else
+      previous->NextNode = current->NextNode;
+    free(current);
+  }
+  return list->Head;
+}
+
 // 노드 삽입 함수
+Node* InsertNode(Node)
+{
+
+}
 
 
 
--- 리스트/head.h
+++ 리스트/head.h
@@ -1,9 +1,15 @@
 #ifndef HEAD_H
 #define HEAD_H
 
-typedef struct Node{
+typedef struct Node
+{
   int data; // 정수형 데이터가 담길 공간
   struct Node* NextNode; // 다음 노드. 자기 자신을 가리킴
 } Node;
+
+typedef struct LinkedList
+{
+  Node* Head;
+} LinkedList;
 
 #endif
